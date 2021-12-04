3aab7292a9873c7bd11a4f7e9b0282b9efdc4b80
Khamidjon <hamidovhamid1998@gmail.com>
2021-12-04 20:41:51 +0500

    create state and event

--- lib/ui/pages/main/bloc/main_event.dart
+++ lib/ui/pages/main/bloc/main_event.dart
@@ -1,3 +1,12 @@
 import 'package:equatable/equatable.dart';
 
 abstract class MainEvent extends Equatable {}
+
+class GetPageEvent extends MainEvent {
+  final int index;
+
+  GetPageEvent(this.index);
+
+  @override
+  List<Object?> get props => [];
+}
--- lib/ui/pages/main/bloc/main_state.dart
+++ lib/ui/pages/main/bloc/main_state.dart
@@ -6,3 +6,56 @@ class MainInitalState extends MainState {
   @override
   List<Object?> get props => [];
 }
+
+// STATE -------------- PAGES STATE ------------
+abstract class PagesState extends MainState {
+  int get index;
+}
+
+class OrganisationsPageState extends PagesState {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  int get index => 0;
+}
+
+class NewsPageState extends PagesState {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  int get index => 1;
+}
+
+class LeadersPageState extends PagesState {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  int get index => 2;
+}
+
+class SavedMessagesPageState extends PagesState {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  int get index => 3;
+}
+
+class AccountPageState extends PagesState {
+  @override
+  List<Object?> get props => [];
+
+  @override
+  int get index => 4;
+}
+// END -------------- PAGES STATE ------------
+
+// STATE -------------- MESSAGES STATE ------------
+class MessagesPageState extends MainState {
+  @override
+  List<Object?> get props => [];
+}
+// END -------------- MESSAGES STATE ------------
